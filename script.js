8c41fc84b3310f8c0a691ec0ea78e9105061f85b
UnniKrishnan-123 <71590116+UnniKrishnan-123@users.noreply.github.com>
2022-03-22 19:30:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function themetoggle(){
+    let cutt_stylesheet = document.
+    getElementById("dark");
+    if (curr_stylesheet.href.match("style.css")){
+        curr_stylesheet.href ="lightmode.css";
+
+    }
+    else {curr_stylesheet.href ="style.css";}
+
+
+}
